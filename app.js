10c60b4ca60058d90a9b6ffc230b5bcd4eabfeba
Nicolette <nnicky8902gmail.com>
2025-05-20 11:34:31 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,63 @@
+const todoForm = document.querySelector('form');
+const todoInput = document.getElementById('todo-input');
+const todoListUL = document.getElementById('todo-list');
+
+let allTodos = getTodos();
+updateTodoList();
+
+todoForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  addTodo();
+});
+
+function addTodo() {
+  const todoText = todoInput.value.trim();
+  if (todoText.length > 0) {
+    allTodos.push(todoText);
+    saveTodos();
+    updateTodoList();
+    todoInput.value = '';
+  }
+}
+
+function updateTodoList() {
+  todoListUL.innerHTML = '';
+  allTodos.forEach((todo, todoIndex) => {
+    const todoItem = createTodoItem(todo, todoIndex);
+    todoListUL.append(todoItem);
+  });
+}
+
+function createTodoItem(todo, todoIndex) {
+  const todoId = 'todo-' + todoIndex;
+  const todoLI = document.createElement('li');
+  todoLI.className = 'todo';
+
+  todoLI.innerHTML = `
+    <input type="checkbox" id="${todoId}">
+    <label for="${todoId}" class="custom-checkbox"></label>
+    <label for="${todoId}" class="todo-text">${todo}</label>
+    <button class="delete-button"><i class="fas fa-trash"></i></button>
+  `;
+
+  const deleteButton = todoLI.querySelector('.delete-button');
+  deleteButton.addEventListener('click', () => {
+    deleteTodoItem(todoIndex);
+  });
+
+  return todoLI;
+}
+
+function deleteTodoItem(index) {
+  allTodos = allTodos.filter((_, i) => i !== index);
+  saveTodos();
+  updateTodoList();
+}
+
+function saveTodos() {
+  localStorage.setItem('todos', JSON.stringify(allTodos));
+}
+
+function getTodos() {
+  return JSON.parse(localStorage.getItem('todos') || '[]');
+}
